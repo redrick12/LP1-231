c8997e4933d3b0f164f69d2e41eae8f62fdb1f8f
redrick12 <erickverissimodasilva144@gmail.com>
2023-03-07 15:21:48 -0300

    novo commit

---
+++ src/semana03/introducao.java
@@ -0,0 +1,79 @@
+package semana03;
+
+import java.util.Scanner;
+
+public class introducao {
+    public static void main (string[] args){
+/*comentario
+ * de
+ * multiplas
+ * linhas
+ * 
+ */
+   byte n1= 100;    //-128 a 127
+   short n2=3000; //-327 127
+        short n2=3000; //-32768 a 32767
+        int n3= 40000000; //2 bi
+        byte n1= 100;    //-128 a 127
+             short n2=3000; //-32768 a 32767
+             int n3= 40000000; //2 bilhoes 
+             long n4=32332332323323232L; //8 bytes
+     
+             //ponto flutuante 
+             // float, double 
+             float nota1=9.5f; //4 bytes
+             double nota2=9.5f; 
+             byte n1= 100;    //-128 a 127
+                  short n2=3000; //-32768 a 32767
+                  int n3= 40000000; //2 bilhoes 
+                  long n4=32332332323323232L; //8 bytes
+          
+                  //ponto flutuante 
+                  // float, double 
+                  float nota1=9.5f; //4 bytes
+                  byte n1= 100;    //-128 a 127
+                    {
+    public static void     short n2=3000; //-32768 a 32767
+                       int n3= 40000000; //2 bilhoes 
+                       long n4=32332332323323232L; //8 bytes
+               
+                       //ponto flutuante 
+                       // float, double 
+                       float nota1=9.5f; //4 bytes
+                       double nota2=9.5f; //8 bytes
+        double nota2=9.5f; //8 bytes
+
+        //char 
+        char=letra= 'A'
+        {
+            System.out.println(palavra.);
+
+            //variaveis e constantes 
+            //tipo nome = valor;
+            // tipo nome;
+            // nome = valor;
+
+            short idade = 30;
+
+            short idade2;
+            idade = 30;
+
+            //Java 10 - inferencia de tipos 
+            var sobrenome = "Santos";
+            var idade3 = 25;
+
+//constantes 
+final double TESTE = 30.4;
+
+//Entrada e saida de dados 
+Scanner sc= new Scanner (System.in);
+
+System.out.println("Entre com o seu nome");
+String NomeInformado=sc.nextline();
+
+System.out.println("Entre com sua idade");
+short IdadeInformada=sc.nextShort();
+
+System.out.println(NomeInformado + " " + IdadeInformada);
+
+sc.close(); 
