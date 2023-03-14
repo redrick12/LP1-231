3336cfd0424c2e03ec87918f094615c169dc1be5
vasco da gama <erickverissimodasilva144@gmail.com>
2023-03-14 15:08:07 -0300

    estruturascontrole

---
+++ src/semana04/EstruturaControle.java
@@ -0,0 +1,35 @@
+package semana04;
+
+public class EstruturasControle{
+    public static void main(String[] args) {
+        //if 
+       final int var ´maioridade´ = 19;
+        if (idade>=maioridade) {
+            System.out.println("Maior de idade");
+            
+        }
+    }
+    var ligado = true;
+    if (ligado){
+        System.out.println("esta ligado");
+        else {
+            System.out.println(esta desligado);
+        }
+    }
+    switch (n) {
+        case 1:
+        n+1;
+            
+            break;
+    
+        default:
+            break;
+    }
+   // loop 
+   int contador=0;
+   for 
+}
+String [] nomes = new String [3];
+nomes[0] = "Maria";
+nomes[1] = "Marta";
+nomes[2] = "Marcia"'git '
